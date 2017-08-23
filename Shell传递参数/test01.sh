d0f33fffdce1beafe6e3207d31fdb0e93345911d
piaolaoshi_ubuntu <piaolaoshi_ubuntu@example.com>
2017-08-23 12:28:59 +0800

    Shell传递参数

--- Shell传递参数/test01.sh
+++ Shell传递参数/test01.sh
@@ -1,65 +1,26 @@
 #!/bin/bash
-echo  "Hello World !"
-echo -e
-# #! 是一个约定的标记，它告诉系统这个脚本需要什么解释器来执行，即使用哪一种 Shell。
-#echo 命令用于向窗口输出文本。 
-
-:<<!
-echo "多行注解"
-!
-
-#将/etc下目录文件名循环出来
-:<<!
-for file in `ls /etc` 
-do
-	echo $file
-done
-!
+echo "执行文件名： $0"
+echo "第一个参数： $1"
+echo "第二个参数： $2"
 echo -e
 
-your_name="piaolaoshi"
-#echo $your_name
-echo ${your_name}
-str="hi \"$your_name\""
-echo $str $your_name ${#str}
-#使用 readonly 命令可以将变量定义为只读变量，只读变量的值不能被改变。
-#readonly your_name
-
-#查找字符 s 的位置，没有成功输出，一直输出0
-echo `expr index "your_name" s`
-
-your_name="linshusen"
-#变量被删除后不能再次使用。unset 命令不能删除只读变量。
-#unset your_name
-echo ${your_name:3:5}
-
-echo -e
-
+echo "参数个数为： $#"
+echo "传递的参数作为一个字符串显示： $*"
+#echo "传递的参数作为一个字符串显示： $@"
 :<<!
-for x in one two three
-do
-	echo number $x
-done
+$* 与 $@ 区别：
 
-for x in /var/log/*
-do
-	#echo "$x is a file living in /var/log"
-	echo "$(basename $x) is a file living in /var/log"
-done
-
-这个$x获得的是绝对路径文件名；可以使用 “basename” 可执行程序来除去前面的路径信息。
-如果只引用当前工作目录中的文件（例如，如果输入 “for x in *”），则产生的文件列表将没有路径信息的前缀。
+    相同点：都是引用所有参数。
+    不同点：只有在双引号中体现出来。假设在脚本运行时写了三个参数 1、2、3，，则 " * " 等价于 "1 2 3"（传递了一个参数），而 "@" 等价于 "1" "2" "3"（传递了三个参数）。
 !
+echo -e
+echo "--\$*演示---"
+for i in "$*"
+do 
+    echo $i
+done
 
-array_name=(
-2
-3
-1
-4
-)
-echo ${array_name[2]}
-echo ${array_name[@]}
-length1=${#array_name[@]}
-length2=${#array_name[*]}
-lengthn=${#array_name[9]}
-echo $lengthn
+echo "---\$@演示--"
+for i in "$@";do
+    echo $i
+done
